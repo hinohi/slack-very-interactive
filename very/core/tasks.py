6e2c7c4b287b159232b6a25c0d349888d28c1171
Daiju Nakayama <42.daiju@gmail.com>
2018-04-04 20:58:28 +0900

    task にエラーハンドリング追加

--- very/core/tasks.py
+++ very/core/tasks.py
@@ -7,10 +7,24 @@ _logger = getLogger(__name__)
 celery = Celery(__name__)
 
 
-def task(name, **kwargs):
+def task(name, **options):
     def wrapper(func):
-        _logger.info('register task: name=%s option=%s', name, kwargs)
-        return celery.task(name=name, **kwargs)(func)
+        from functools import wraps
+
+        _logger.info('register task: name=%s options=%s', name, options)
+
+        @wraps(func)
+        def inner(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except Exception as e:
+                import traceback
+                _logger.error("unbounded error occur calling: %s\n%s",
+                              e, traceback.format_exc())
+                return traceback.extract_stack()
+
+        return celery.task(name=name, **options)(inner)
+
     return wrapper
 
 
